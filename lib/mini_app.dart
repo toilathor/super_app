b94a76d7c51e6b4489b51391a97a8af44092f839
phipv <phamphi@Fi.local>
2025-05-27 10:20:28 +0700

    add logic loading

--- lib/mini_app.dart
+++ lib/mini_app.dart
@@ -2,10 +2,12 @@ class MiniApp {
   final String id;
   final String name;
   final String link;
+  final bool isEnable;
 
   MiniApp({
     required this.id,
     required this.name,
     required this.link,
+    required this.isEnable,
   });
 }
